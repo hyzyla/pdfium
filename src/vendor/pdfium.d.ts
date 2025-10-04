186c417fb3ff37d1287f94bf6b3699c665751d01
Yevhenii Hyzyla <hyzyla@gmail.com>
2025-10-04 10:03:31 +0000

    Update PDFium

--- src/vendor/pdfium.d.ts
+++ src/vendor/pdfium.d.ts
@@ -1,6 +1,10 @@
 export declare type PDFium = {
   _FPDF_InitLibraryWithConfig: (config: object) => void;
-  _FPDF_LoadMemDocument: (documentPtr: number, documentSize: number, passwordPtr: number) => number;
+  _FPDF_LoadMemDocument: (
+    documentPtr: number,
+    documentSize: number,
+    passwordPtr: number,
+  ) => number;
   _FPDF_GetLastError: () => number;
   _FPDF_DestroyLibrary: () => void;
   _FPDF_GetPageCount: (documentIdx: number) => number;
@@ -11,11 +15,24 @@ export declare type PDFium = {
   _FPDFPage_GetObject: (page: number, i: number) => number;
   _FPDFPageObj_GetType: (objectIdx: number) => number;
   _FPDFImageObj_GetBitmap: (objectIdx: number) => number;
-  _FPDFImageObj_GetImageDataRaw: (objectIdx: number, buffer: number, lenght: number) => number;
+  _FPDFImageObj_GetImageDataRaw: (
+    objectIdx: number,
+    buffer: number,
+    lenght: number,
+  ) => number;
   _FPDF_ClosePage: (page: number) => void;
-  _FPDFImageObj_GetImagePixelSize: (objectIdx: number, width: number, height: number) => number;
+  _FPDFImageObj_GetImagePixelSize: (
+    objectIdx: number,
+    width: number,
+    height: number,
+  ) => number;
   _FPDFImageObj_GetImageFilterCount(objectIdx: number): number;
-  _FPDFImageObj_GetImageFilter(objectIdx: number, index: number, buffer: number, length: number): number;
+  _FPDFImageObj_GetImageFilter(
+    objectIdx: number,
+    index: number,
+    buffer: number,
+    length: number,
+  ): number;
   _FPDF_FFLDraw: (
     form_handle: number,
     bitmap: number,
@@ -27,16 +44,30 @@ export declare type PDFium = {
     rotate: number,
     flags: number,
   ) => void;
-  _FPDFDOC_InitFormFillEnvironment: (document: number, formHandle: number) => number;
+  _FPDFDOC_InitFormFillEnvironment: (
+    document: number,
+    formHandle: number,
+  ) => number;
   _FPDFDOC_ExitFormFillEnvironment: (formHandle: number) => void;
   _FORM_OnAfterLoadPage: (page: number, formHandle: number) => void;
   _FORM_OnBeforeClosePage: (page: number, formHandle: number) => void;
   _FPDFText_LoadPage: (page: number) => number;
   _FPDFText_ClosePage: (textPage: number) => void;
   _FPDFText_CountChars: (textPage: number) => number;
-  _FPDFText_GetText: (textPage: number, startIndex: number, count: number, buffer: number) => number;
+  _FPDFText_GetText: (
+    textPage: number,
+    startIndex: number,
+    count: number,
+    buffer: number,
+  ) => number;
 
-  _FPDFBitmap_CreateEx: (width: number, height: number, format: number, ptr: number, stride: number) => number;
+  _FPDFBitmap_CreateEx: (
+    width: number,
+    height: number,
+    format: number,
+    ptr: number,
+    stride: number,
+  ) => number;
   _FPDFBitmap_FillRect: (
     bitmap: number,
     left: number,
@@ -57,7 +88,11 @@ export declare type PDFium = {
   ) => void;
   _FPDFBitmap_Destroy: (bitmap: number) => void;
   _FPDFBitmap_GetBuffer: (bitmap: number) => number;
-  _FPDFImageObj_GetRenderedBitmap: (document: number, page: number, object: number) => number;
+  _FPDFImageObj_GetRenderedBitmap: (
+    document: number,
+    page: number,
+    object: number,
+  ) => number;
   _FPDFBitmap_GetWidth: (bitmap: number) => number;
   _FPDFBitmap_GetHeight: (bitmap: number) => number;
   _FPDFBitmap_GetStride: (bitmap: number) => number;
